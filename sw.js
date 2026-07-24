1f049b5e63b0e0e639e362122cb8244951928317
Joel Nuñez Lazo <151790427+iHe1998@users.noreply.github.com>
2026-07-24 19:40:47 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE = 'agrupadora-v5';
+const CACHE = 'agrupadora-v6';
 const ASSETS = [
   '/Extraccion-agrupadora/',
   '/Extraccion-agrupadora/index.html',
   '/Extraccion-agrupadora/manifest.json',
   '/Extraccion-agrupadora/lector-etiquetas.html',
   '/Extraccion-agrupadora/logo.svg',
-  'https://cdn.jsdelivr.net/npm/jsqr@1.4.0/dist/jsQR.min.js'
+  'https://cdn.jsdelivr.net/npm/jsqr@1.4.0/dist/jsQR.min.js',
   'https://cdn.jsdelivr.net/npm/@zxing/browser@0.1.4/esm/index.js'
 ];
 
